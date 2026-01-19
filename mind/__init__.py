2df4db8070953ff85f7774e2dbe1f04413e2bef0
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-19 23:57:32 +0000

    Update __init__.py

---
+++ mind/__init__.py
@@ -0,0 +1,6 @@
+# sandys_law_a7do/mind/__init__.py
+
+from .coherence import CoherenceReport, compute_coherence
+from .perception import PerceptSummary, summarize_perception
+from .regulation import RegulationDecision, regulate
+from .preference import PreferenceEngine, PreferenceStore, PreferenceConfig
