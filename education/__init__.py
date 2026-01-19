6ed47a604b62d96c7468e103a725a12f29d413c0
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-19 23:44:20 +0000

    Update __init__.py

---
+++ education/__init__.py
@@ -0,0 +1,6 @@
+# sandys_law_a7do/education/__init__.py
+
+from .curriculum import Curriculum
+from .exams import Exam, ExamResult
+from .readiness import readiness_score
+from .cv import CurriculumVitae
