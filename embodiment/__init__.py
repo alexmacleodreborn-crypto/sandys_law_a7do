945911bec6c704aaf33e00f398625aaa01861a66
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-20 00:06:48 +0000

    Update __init__.py

---
+++ embodiment/__init__.py
@@ -0,0 +1,6 @@
+# sandys_law_a7do/embodiment/__init__.py
+
+from .boundaries import BoundaryState, BoundarySignal
+from .ownership import OwnershipMap
+from .thermal_pain import ThermalPainSignal
+from .ledger import EmbodimentLedger
