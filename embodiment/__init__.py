019a059711dbe4fc45c108e9b74b3966ac01baab
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-22 22:27:16 +0000

    Update __init__.py

--- embodiment/__init__.py
+++ embodiment/__init__.py
@@ -1,3 +1,4 @@
-from .entry import LedgerEntry
+from .boundaries import BoundaryState
+from .ownership import OwnershipMap
+from .thermal_pain import ThermalPain
 from .ledger import EmbodimentLedger
-from .invariants import EmbodimentKind
