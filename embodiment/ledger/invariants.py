4964a72f94f2cc5505b009fd4dafe6160ba83908
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-22 22:21:02 +0000

    Create invariants.py

---
+++ embodiment/ledger/invariants.py
@@ -0,0 +1,12 @@
+# embodiment/ledger/invariants.py
+
+from typing import Literal
+
+
+EmbodimentKind = Literal[
+    "boundary",
+    "thermal_region",
+    "pain_surface",
+    "scuttling_skill",
+    "body_ownership",
+]
