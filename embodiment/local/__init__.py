6406dcb528d4738d3dc2ca03e182c649b6aaf354
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-23 00:06:25 +0000

    Create __init__.py

---
+++ embodiment/local/__init__.py
@@ -0,0 +1,13 @@
+"""
+Local Embodiment Layer
+
+This package implements:
+- local body ownership
+- reflex buffering
+- local copilot regulation
+- consolidation into embodied invariants
+
+No cognition.
+No symbols.
+No planning.
+"""
