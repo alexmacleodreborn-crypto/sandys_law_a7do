f0577528bd53226d1e1902e00cd4e96175002b40
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-01-25 14:34:39 +0000

    Update __init__.py

--- interfaces/chat/__init__.py
+++ interfaces/chat/__init__.py
@@ -1,1 +1,11 @@
+# sandys_law_a7do/interfaces/chat/__init__.py
 
+"""
+Chat interfaces for A7DO.
+
+IMPORTANT:
+- Chat is OBSERVER ONLY
+- No state mutation
+- No action selection
+- No memory writes
+"""
